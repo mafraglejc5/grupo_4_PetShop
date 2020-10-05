9c6596433383d73c2241f75016591ab9441eeb4f
MendietaSergio <mendieta.sergio.1995@gmail.com>
2020-10-05 19:55:46 -0300

    añade arreglos redirectProfile

--- PetShop/redirect/redirectProfile.js
+++ PetShop/redirect/redirectProfile.js
@@ -3,7 +3,6 @@ SI EL INVITADO QUIERE ENTRAR EN LA RUTA
 /users/profile
 LO REDIRECCIONA PARA LOGEARSE.
 */
-modul
 module.exports = function redirectProfile(req,res,next){
     if(!req.session.user){
         res.redirect('/users/login');
